6f798fd037e0ec07a170d8dbf4f775ed938b82c1
ayush mishra <ayushmishra0810@gmail.com>
2020-12-12 22:11:48 +0530

    Updates

--- src/app/home/contacts/contacts.module.ts
+++ src/app/home/contacts/contacts.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { ContactsPageRoutingModule } from './contacts-routing.module';
 
 import { ContactsPage } from './contacts.page';
+import { Contacts } from '@ionic-native/contacts/ngx';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { ContactsPage } from './contacts.page';
     IonicModule,
     ContactsPageRoutingModule
   ],
-  declarations: [ContactsPage]
+  declarations: [ContactsPage],providers:[Contacts]
 })
 export class ContactsPageModule {}
--- src/app/home/contacts/contacts.page.ts
+++ src/app/home/contacts/contacts.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contacts } from '@ionic-native/contacts/ngx';
 
 @Component({
   selector: 'app-contacts',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private contact:Contacts) { }
+userContacts
   ngOnInit() {
+    
   }
 
 }
