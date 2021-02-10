a791cefa21b9117d3b0f29ac7de86b3ca355bf9c
ayush mishra <ayushmishra0810@gmail.com>
2021-02-11 02:29:11 +0530

    Updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyB9YFBnm7kUDdveRTPHkivZFmhRO8hjK8k",
+    authDomain: "letschat-3f883.firebaseapp.com",
+    projectId: "letschat-3f883",
+    storageBucket: "letschat-3f883.appspot.com",
+    messagingSenderId: "628692925534",
+    appId: "1:628692925534:web:00a0c60548f5428ccc88cf",
+    measurementId: "G-NS37ZNV576"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyB9YFBnm7kUDdveRTPHkivZFmhRO8hjK8k",
+    authDomain: "letschat-3f883.firebaseapp.com",
+    projectId: "letschat-3f883",
+    storageBucket: "letschat-3f883.appspot.com",
+    messagingSenderId: "628692925534",
+    appId: "1:628692925534:web:00a0c60548f5428ccc88cf",
+    measurementId: "G-NS37ZNV576"
+  }
 };
 
 /*
