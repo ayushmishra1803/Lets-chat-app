e0898f5430ebf408794ac65aed7abf15642a340b
ayush mishra <ayushmishra0810@gmail.com>
2021-02-13 02:57:29 +0530

    Updates

--- src/app/auth/sign-up/sign-up.module.ts
+++ src/app/auth/sign-up/sign-up.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { SignUpPageRoutingModule } from './sign-up-routing.module';
 
 import { SignUpPage } from './sign-up.page';
+import { UsernameValidator } from 'src/app/validators/userNameValidator';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SignUpPageRoutingModule
-  ],
-  declarations: [SignUpPage]
+    SignUpPageRoutingModule,ReactiveFormsModule,AngularFirestoreModule  ],
+  declarations: [SignUpPage,],providers:[UsernameValidator]
 })
 export class SignUpPageModule {}
--- src/app/auth/sign-up/sign-up.page.ts
+++ src/app/auth/sign-up/sign-up.page.ts
@@ -1,27 +1,60 @@
 import { Component, OnInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { FormControl, FormGroup, NgForm, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { LoginService } from "src/app/Service/login/login.service";
-
+import { UsernameValidator } from "src/app/validators/userNameValidator";
 @Component({
   selector: "app-sign-up",
   templateUrl: "./sign-up.page.html",
   styleUrls: ["./sign-up.page.scss"],
 })
 export class SignUpPage implements OnInit {
-  constructor(private router: Router, private loginService: LoginService) {}
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private angularFire: AngularFirestore
+  ) {}
 
-  ngOnInit() {}
-  SignUp(f: NgForm) {
+  userForm: FormGroup;
+  ngOnInit() {
+    this.userForm = new FormGroup({
+      first_name: new FormControl("", [
+        Validators.required,
+        UsernameValidator.username(this.angularFire),
+      ]),
+
+      last_name: new FormControl("", {
+        asyncValidators: [],
+      }),
+      username: new FormControl("", {
+        asyncValidators: [],
+      }),
+
+      mobile_number: new FormControl("", {
+        asyncValidators: [],
+      }),
+
+      email: new FormControl("", {
+        asyncValidators: [],
+      }),
+
+      password: new FormControl("", {
+        asyncValidators: [],
+      }),
+    });
+  }
+  SignUp() {
     const data = {
-      first_name: f.value.firstName,
-      last_name: f.value.lastName,
-      username: f.value.username,
-      mobile_number: f.value.mobilenumber,
-      email: f.value.email,
-      password: f.value.password,
+      first_name: this.userForm.controls.first_name.value,
+      last_name: this.userForm.controls.last_name.value,
+      username: this.userForm.controls.username.value,
+      mobile_number: this.userForm.controls.mobile_number.value,
+      email: this.userForm.controls.email.value,
+      password: this.userForm.controls.password.value,
     };
-    this.loginService.userSiginUp(data)
+
+    this.loginService.userSiginUp(data);
   }
   navigateTologin() {
     this.router.navigate(["/login"]);
