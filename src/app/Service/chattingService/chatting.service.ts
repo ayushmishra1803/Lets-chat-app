23ac979db6abd3e93749c6c03823b84fc7cb4363
Ayush Mishra <ayushmishra0810@gmail.com>
2021-03-21 18:52:50 +0530

    Changes

--- src/app/Service/chattingService/chatting.service.ts
+++ src/app/Service/chattingService/chatting.service.ts
@@ -10,5 +10,34 @@ export class ChattingService {
     private angularfire: AngularFirestore,
     private userData: UserDataService
   ) {}
-  addtochats(...agrs) {}
+  addtochats(...agrs) {
+    const chatData = {
+      ...agrs[2],
+    };
+    this.angularfire
+      .collection("chats")
+      .add(chatData)
+      .then((charAdded) => {
+        const dataFor0 = {
+          id: agrs[1].id,
+          firebaseChatId: charAdded.id,
+        };
+        const dataFor1 = {
+          id: agrs[1].id,
+          firebaseChatId: charAdded.id,
+        };
+        this.angularfire
+          .collection(`users/${agrs[0].id}/chats`)
+          .add(dataFor0)
+          .then((result) => {
+            this.angularfire
+              .collection(`users/${agrs[1].id}/chats`)
+              .add(dataFor1)
+              .then((result) => {});
+          });
+      });
+  }
+  addMessgaesIfChatExist(chatId) {
+ 
+  }
 }
