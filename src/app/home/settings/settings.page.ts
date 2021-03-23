97c841dd90a0619ff522a2b7cd4f1efdf9a79d8f
Ayush Mishra <ayushmishra0810@gmail.com>
2021-03-24 02:26:10 +0530

    Logout Added

--- src/app/home/settings/settings.page.ts
+++ src/app/home/settings/settings.page.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { UserDataService } from 'src/app/Service/userData/user-data.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { LoginService } from "src/app/Service/login/login.service";
+import { UserDataService } from "src/app/Service/userData/user-data.service";
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.page.html',
-  styleUrls: ['./settings.page.scss'],
+  selector: "app-settings",
+  templateUrl: "./settings.page.html",
+  styleUrls: ["./settings.page.scss"],
 })
 export class SettingsPage implements OnInit {
-
-  constructor(private userData:UserDataService) { }
-  activeUserData:any={}
+  constructor(
+    private userData: UserDataService,
+    private router: Router,
+    private login: LoginService
+  ) {}
+  activeUserData: any = {};
 
   ngOnInit() {
-    this.activeUserData=this.userData.getUserData();
+    this.activeUserData = this.userData.getUserData();
   }
-
+  logOut() {
+    this.userData.setuserData(null);
+    this.userData.setLoginStatus(false);
+    this.router.navigate(["/login"], { replaceUrl: true });
+  }
 }
