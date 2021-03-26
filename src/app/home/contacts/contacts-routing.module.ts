5db91354745dc8d60dd430ada3c4aad2cf30c9c8
Ayush Mishra <ayushmishra0810@gmail.com>
2021-03-26 17:35:37 +0530

    Chnages

--- src/app/home/contacts/contacts-routing.module.ts
+++ src/app/home/contacts/contacts-routing.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { ContactsPage } from './contacts.page';
+import { ContactsPage } from "./contacts.page";
 
 const routes: Routes = [
   {
-    path: '',
-    component: ContactsPage
-  }
+    path: "",
+    component: ContactsPage,
+  },
 ];
 
 @NgModule({
