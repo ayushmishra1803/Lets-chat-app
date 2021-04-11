d5733742d73077eb5841af0ec877754b7d5505b8
Ayush Mishra <ayushmishra0810@gmail.com>
2021-04-11 20:23:15 +0530

    Realease Ready

--- src/app/home/profile-page/profile-page.page.ts
+++ src/app/home/profile-page/profile-page.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { ViewerModalComponent } from "ngx-ionic-image-viewer";
 import { HomeChatsService } from "src/app/Service/HomeChatService/home-chats.service";
 import { ViewPhotoServiceService } from "src/app/Service/viewPhotoService/view-photo-service.service";
 
@@ -11,13 +13,24 @@ export class ProfilePagePage implements OnInit {
   UserData;
   constructor(
     private homeChattingService: HomeChatsService,
-    private photoViewImage: ViewPhotoServiceService
+    private photoViewImage: ViewPhotoServiceService,
+    private modalController: ModalController
   ) {}
 
   ngOnInit() {
     this.UserData = this.homeChattingService.getUserData();
   }
-  viewImage(src) {
-    this.photoViewImage.viewPhoto(src)
+  async viewImage(src) {
+    const modal = await this.modalController.create({
+      component: ViewerModalComponent,
+      componentProps: {
+        src: src,
+      },
+      cssClass: "ion-img-viewer",
+      keyboardClose: true,
+      showBackdrop: true,
+    });
+
+    modal.present();
   }
 }
